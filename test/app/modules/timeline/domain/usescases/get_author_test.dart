f115f39f4223ab77f6bee46453bb9d0ae4763ab1
Ogaraku Ugochukwu <justineugoogaraku@gmail.com>
2022-09-14 20:09:13 +0100

    chore: added get author tests

--- test/app/modules/timeline/domain/usescases/get_author_test.dart
+++ test/app/modules/timeline/domain/usescases/get_author_test.dart
@@ -1,3 +1,6 @@
+import 'dart:math';
+
+import 'package:clean/app/core/errors/failures.dart';
 import 'package:clean/app/modules/timeline/domain/entities/author_entity.dart';
 import 'package:clean/app/modules/timeline/domain/repositories/timeline_repository.dart';
 import 'package:clean/app/modules/timeline/domain/usescases/get_author.dart';
@@ -17,15 +20,48 @@ void main() {
   });
 
   group('Get Author usecase', () {
+    int userId = 1;
     Author author = Author(
-        id: 1, name: 'name', email: 'email', gender: 'gender', status: 'status');
-    test('Should return an Author', () {
+      id: 1,
+      name: 'name',
+      email: 'email',
+      gender: 'gender',
+      status: 'status',
+    );
+    Failure failure = Failure(code: 404);
+
+    test('should return an Author', () async {
       //Arrange
       when(
         () => mockTimelineRepository.getAuthor(any()),
       ).thenAnswer((_) async => Right(author));
+
       //Act
+      final result = await getAuthor(Params(userId: userId));
+
       //Assert
+      verify(() => mockTimelineRepository.getAuthor(userId)).called(1);
+      verifyNoMoreInteractions(mockTimelineRepository);
+      expect(result, Right(author));
+    });
+
+    test('should return a failure', () async {
+      //Arrange
+      when(
+        () => mockTimelineRepository.getAuthor(
+          any(),
+        ),
+      ).thenAnswer(
+        (_) async => Left(failure),
+      );
+
+      //Act
+      final result = await getAuthor(Params(userId: userId));
+
+      //assert
+      verify(() => mockTimelineRepository.getAuthor(userId)).called(1);
+      verifyNoMoreInteractions(mockTimelineRepository);
+      expect(result, Left(failure));
     });
   });
 }
